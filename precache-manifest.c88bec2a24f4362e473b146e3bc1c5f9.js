d662e09453d26b5ef19c4ce88ef06b4b4fe7fde9
syed-zishan <syedzishan0591@gmail.com>
2020-05-02 04:02:20 +0530

    Updates

---
+++ precache-manifest.c88bec2a24f4362e473b146e3bc1c5f9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa73c39eb150601f791e86a66cdbc99f",
+    "url": "/syed-zishan/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "7f9a79da934c078c0560",
+    "url": "/syed-zishan/monsters-rolodex/static/css/main.0645dc17.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/syed-zishan/monsters-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/syed-zishan/monsters-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7f9a79da934c078c0560",
+    "url": "/syed-zishan/monsters-rolodex/static/js/main.e53894fe.chunk.js"
+  },
+  {
+    "revision": "7379bfcf714adee69f45",
+    "url": "/syed-zishan/monsters-rolodex/static/js/runtime-main.6e506506.js"
+  }
+]);
